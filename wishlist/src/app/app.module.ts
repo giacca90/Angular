47fc1de954f2b84ea1c923fe91a980970a8a0bf9
Giacca90 <giacca90mobile@gmail.com>
2023-05-25 14:39:57 +0200

    Introducción al manejo de rutas

--- wishlist/src/app/app.module.ts
+++ wishlist/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DestinosViajeComponent } from './destinos-viaje/destinos-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
+import { DestinoDettalleComponent } from './destino-dettalle/destino-dettalle.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'Home', pathMatch: 'full'},
+  {path: 'Home', component: ListaDestinosComponent},
+  {path: 'Destino', component: DestinoDettalleComponent},
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DestinosViajeComponent,
-    ListaDestinosComponent
+    ListaDestinosComponent,
+    DestinoDettalleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ wishlist/src/app/destino-dettalle/destino-dettalle.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destino-dettalle',
+  templateUrl: './destino-dettalle.component.html',
+  styleUrls: ['./destino-dettalle.component.css']
+})
+export class DestinoDettalleComponent {
+
+}

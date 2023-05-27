44e47645cca49d49208b7e41c10ee62611e2fd6c
Giacca90 <giacca90mobile@gmail.com>
2023-05-27 09:50:19 +0200

    Principios de diseño de Observables y RxJS

--- wishlist/src/app/models/destinos-api-client.model.ts
+++ wishlist/src/app/models/destinos-api-client.model.ts
@@ -1,7 +1,9 @@
+import { BehaviorSubject, Observer, Subject } from 'rxjs';
 import { DestinoViaje } from './destino-viaje.model';
 
 export class DestinosApiClient {
 	destinos:DestinoViaje[];
+	current: Subject<DestinoViaje> = new BehaviorSubject<DestinoViaje>(null);
 	constructor() {
        this.destinos = [];
 	}
@@ -14,4 +16,12 @@ export class DestinosApiClient {
 	getById(id:String | null):DestinoViaje{
 	  return this.destinos.filter(function(d){return d.id.toString() == id;})[0];
     }
+	elegir(d: DestinoViaje)  {
+		this.destinos.forEach(x => x.setSelected(false));
+		d.setSelected(true);
+		this.current.next(d);
+	}
+	subscribeOnChange(fn: Partial<Observer<DestinoViaje>> | ((value: DestinoViaje) => void) | undefined)  {
+		this.current.subscribe(fn);
+	}
 }

23bc63a19e0a5313d9a0627bc6ea964c43e4ed78
Giacca90 <giacca90mobile@gmail.com>
2023-07-10 12:42:54 +0200

    manejo de rutas avanzado

--- angular-whishlist/src/app/app.module.ts
+++ angular-whishlist/src/app/app.module.ts
@@ -8,20 +8,30 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 
 import { AppComponent } from './app.component';
-import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
-import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
-import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+import { DestinoViajeComponent } from './components/destino-viaje/destino-viaje.component';
+import { ListaDestinosComponent } from './components/lista-destinos/lista-destinos.component';
+import { DestinoDetalleComponent } from './components/destino-detalle/destino-detalle.component';
 import { DestinosApiClient } from './models/destinos-api-client.model';
-import { FormDestinoViajeComponent } from './form-destino-viaje/form-destino-viaje.component';
+import { FormDestinoViajeComponent } from './components/form-destino-viaje/form-destino-viaje.component';
 import { DestinosViajesState,
          intializeDestinosViajesState,
          reducerDestinosViajes,
          DestinosViajesEffects } from './models/destinos-viajes-state.model';
+import { LoginComponent } from './components/login/login/login.component';
+import { ProtectedComponent } from './components/protected/protected/protected.component';
+import { AuthService } from './services/auth.service';
+import { UsuarioLogueadoGuard } from './guards/usuario-logueado/usuario-logueado.guard';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: ListaDestinosComponent },
-    { path: 'destino/:id', component: DestinoDetalleComponent }
+    { path: 'destino/:id', component: DestinoDetalleComponent },
+    { path: 'login', component: LoginComponent },
+    {
+      path: 'protected',
+      component: ProtectedComponent,
+      canActivate: [ UsuarioLogueadoGuard ]
+    }
   ];
 
 // redux init
@@ -44,7 +54,9 @@ const reducersInitialState = {
     DestinoViajeComponent,
     ListaDestinosComponent,
     DestinoDetalleComponent,
-    FormDestinoViajeComponent
+    FormDestinoViajeComponent,
+    LoginComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
@@ -56,7 +68,7 @@ const reducersInitialState = {
     StoreDevtoolsModule.instrument()
   ],
   providers: [
-    DestinosApiClient
+    DestinosApiClient, AuthService, UsuarioLogueadoGuard
   ],
   bootstrap: [AppComponent]
 })
